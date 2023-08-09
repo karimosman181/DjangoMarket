00a3b0e993d515d3bf8ef66ad49fb3595ecc57bf
Karim Osman <kareemosman199@gmail.com>
2023-08-09 21:30:40 +0300

    add dashbourd app

---
+++ djangomarket/dashbourd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DashbourdConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashbourd'
---
+++ djangomarket/dashbourd/views.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.models import login_required
+from django.shortcuts import render
+from item.models import Item
+# Create your views here.
