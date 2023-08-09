8605a70e2bfd40ee8696b4622fd99c085cf3d2df
Karim Osman <kareemosman199@gmail.com>
2023-08-09 21:33:25 +0300

    add view to dashbourd

--- djangomarket/dashbourd/views.py
+++ djangomarket/dashbourd/views.py
@@ -2,3 +2,12 @@ from django.contrib.auth.models import login_required
 from django.shortcuts import render
 from item.models import Item
 # Create your views here.
+
+
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+
+    return render(request , 'dashbourd/index.html', {
+        'items':items,
+    })
