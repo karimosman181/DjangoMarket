c8f8ae3e5e127c0a38d10283ca271ae8eb718f53
Karim Osman <kareemosman199@gmail.com>
2023-08-09 21:41:28 +0300

    edit dashboard

--- djangomarket/dashbourd/views.py
+++ djangomarket/dashbourd/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import login_required
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from item.models import Item
 # Create your views here.
